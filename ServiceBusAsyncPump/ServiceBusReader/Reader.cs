547da871920794b90f0b8872b6f4abc9407030b3
Milo Wical <Milo.Wical@compare.com>
2018-10-12 15:58:07 -0400

    POC Complete.

---
+++ ServiceBusAsyncPump/ServiceBusReader/Reader.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ServiceBusReader
+{
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using Microsoft.Azure.ServiceBus;
+    using Microsoft.Extensions.Configuration;
+
+    public class Reader
+    {
+        public static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var threadCount = int.Parse(config["ListeningThreads"]);
+
+            for (var i = 1; i <= threadCount; i++)
+            {
+                var closure = i;
+
+                Task.Run(() =>
+                {
+                    var id = closure;
+
+                    var client = new QueueClient(config["ConnectionString"], config["QueuePath"]);
+
+                    client.RegisterMessageHandler((message, _) =>
+                    {
+                        Console.WriteLine($"\n[{id}]Received message: {Encoding.UTF8.GetString(message.Body)}");
+
+                        return Task.CompletedTask;
+                    },
+                    new MessageHandlerOptions(_ => Task.CompletedTask)
+                    {
+                        AutoComplete = true
+                    }
+                    );
+
+                    Thread.Sleep(Timeout.Infinite);
+                });
+
+                Console.WriteLine($"Thread {closure}/{threadCount} configured and running...");
+            }
+
+            Console.WriteLine("Configuration complete. Listening for messages...");
+
+            Thread.Sleep(Timeout.Infinite);
+        }
+    }
+}
---
+++ ServiceBusAsyncPump/ServiceBusWriter/Writer.cs
@@ -0,0 +1,33 @@
+﻿namespace ServiceBusWriter
+{
+    using System;
+    using System.Text;
+    using Microsoft.Azure.ServiceBus;
+    using Microsoft.Extensions.Configuration;
+
+    public class Writer
+    {
+        public static void Main(string[] args)
+        {
+            IConfiguration config = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var client = new QueueClient(config["ConnectionString"], config["QueuePath"]);
+
+            Console.Write("Enter a message to transmit (nothing to quit): ");
+
+            var input = Console.ReadLine();
+
+            while (!string.IsNullOrEmpty(input))
+            {
+                client.SendAsync(new Message(Encoding.UTF8.GetBytes(input))).GetAwaiter().GetResult();
+                Console.WriteLine("Message sent.");
+
+                Console.Write("\nEnter a message to transmit (nothing to quit): ");
+
+                input = Console.ReadLine();
+            } 
+        }
+    }
+}
